14a8206155f9cdc30af87d191b937052931949cb
Thibaut CHAUFFIER <tchauffi@gmail.com>
2025-10-22 23:27:46 +0200

    Add Ray struct with methods for creation and formatting

---
+++ src/ray.rs
@@ -0,0 +1,24 @@
+use crate::vec3::Vec3; 
+
+pub struct Ray {
+    pub origin: Vec3,
+    pub direction: Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: Vec3, direction: Vec3) -> Self {
+        Ray { origin, direction }
+    }
+}
+
+impl Ray {
+    pub fn at(&self, t: f64) -> Vec3 {
+        self.origin + self.direction * t
+    }
+}
+
+impl std::fmt::Display for Ray {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Ray(origin: {}, direction: {})", self.origin, self.direction)
+    }
+}
